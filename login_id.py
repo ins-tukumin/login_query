1978b66322e93e2378f04e6653d56b5505393c7d
ins-tukumin <116026504+ins-tukumin@users.noreply.github.com>
2024-07-17 11:34:23 +0900

    Update login_id.py

--- login_id.py
+++ login_id.py
@@ -6,7 +6,7 @@ login_id = st.text_input("ログインIDを入力してください")
 # ボタンを押したときにQualtricsのアンケートURLにリダイレクト
 if st.button("アンケートに進む"):
     # QualtricsのアンケートURL
-    qualtrics_url = "https://your_qualtrics_survey_url"
+    qualtrics_url = "https://nagoyapsychology.qualtrics.com/jfe/form/SV_1H4rEHXCmTh8RxA"
     # クエリパラメータとしてログインIDを追加
     full_url = f"{qualtrics_url}?login_id={login_id}"
     # リダイレクト
